d59fdbb0a7c7b98bfffd0c6b731a82318133241b
alexandrashelley <alexbshelley@gmail.com>
2022-07-21 14:48:07 +0100

    added comments to extractor method

--- lib/e_extractor.rb
+++ lib/e_extractor.rb
@@ -1,8 +1,8 @@
 def e_extractor(str)
   letters = str.chars # turn string into list of letters
-  e_letters = letters.select { |l| l == "e" }.join
-  non_e_letters = letters.select { |l| l != "e" }.join
-  return "#{e_letters}#{non_e_letters}"
+  e_letters = letters.select { |l| l == "e" }.join # select letter e, then join back to string
+  non_e_letters = letters.select { |l| l != "e" }.join # select all letters that aren't e, then join back to string
+  return "#{e_letters}#{non_e_letters}" # return concatenated strings
 end
 
 

7e73adb9e0ebcdbdfa071dc3b68bdc6c9561f22a
alexandrashelley <alexbshelley@gmail.com>
2022-07-21 10:09:57 +0100

    formatting

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -2,29 +2,28 @@ require 'make_snippet'
 
 RSpec.describe "make_snippet" do
 
-it "returns the first five words and '...' if there are more than five words" do
+  it "returns the first five words and '...' if there are more than five words" do
     expect(make_snippet("this string is more than five words")).to eq "this string is more than..."
-end
+  end
 
-it "fails if no argument is given" do
+  it "fails if no argument is given" do
     expect { (make_snippet("")) }.to raise_error "Please input string"
-end
+  end
 
-it "returns string if there are less than five words" do
+  it "returns string if there are less than five words" do
     expect(make_snippet("two words")).to eq "two words"
-end
+  end
 
-it "fails if input has hello?" do
+  it "fails if input has hello?" do
     expect { (make_snippet("hel?lo")) }.to raise_error "Please input string"
-end
+  end
 
-it "fails if user inputs integers" do
+  it "fails if user inputs integers" do
     expect { (make_snippet(1234)) }.to raise_error "Please input string"
-end
+  end
 
-it "fails if input string is just spaces" do
+  it "fails if input string is just spaces" do
     expect { (make_snippet(" ")) }.to raise_error "Please input string"
-end
-
+  end
 end
 

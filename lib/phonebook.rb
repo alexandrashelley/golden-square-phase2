15e37df01c76bf780e5db4fd60c924acb080e17c
alexandrashelley <alexbshelley@gmail.com>
2022-07-26 19:20:44 +0100

    fixed pesky comma -  half of tests pass

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -1,29 +1,22 @@
 class Phonebook 
+
   def initialize
-    @list_array = [] 
+    @numbers = [] 
   end
 
   def extract(entry)
-  # extracts correctly a phone number from a diary entry 
-# takes entry, looks for a number   
-# fail "number is the wrong length!" if !entry.include? (/[0-9]{9}/)
-  delete_commas = entry.delete(",").split(" ") do |word| word.split(" ")
-  end
-  p delete_commas
-  numbers = delete_commas.each do |word| (word.scan(/[0-9]{9}/))[0]
-  end
-  p numbers
-  @list_array += numbers
-  # (entry.scan(/[0-9]{9}/))[0]
-  p @list_array
-  end
-
-  def list
-    # outputs an array, a list of all the numbers 
-    @list_array
+    entry.scan(/[0-9]{11}/).each do |word| 
+      @numbers << word
+    end
   end
   
+  # fail "number is the wrong length!" if !entry.include? 
+  # (/[0-9]{9}/)
+
+  def list
+    return @numbers
+  end
 end
 
-phonebook = Phonebook.new 
-p phonebook.extract("073456789, 073456781")
+p phonebook = Phonebook.new 
+p phonebook.extract("07123456789, 07123456788, 071234567890")
--- spec/phonebook_spec.rb
+++ spec/phonebook_spec.rb
@@ -13,48 +13,52 @@ RSpec.describe Phonebook do
   it "calling our extraction method with just one 
         phone number extracts that phone number" do
     phonebook = Phonebook.new
-    phonebook.extract("073456789") 
-    expect(phonebook.list).to eq ["073456789"]
+    phonebook.extract("07123456789") 
+    expect(phonebook.list).to eq ["07123456789"]
   end
 
-  #3. calling our extraction method with more than one number, extracts those numbers (as a list)
-  it "calling our extraction method with more than one number, 
-        extracts those numbers (as a list)" do
+  #3. calling our extraction method with more than one 
+  # number, extracts those numbers (as a list)
+  it "calling our extraction method with more than one
+        number, extracts those numbers (as a list)" do
     phonebook = Phonebook.new
-    phonebook.extract("073456789, 073456781")
-    expect(phonebook.list).to eq ["073456789", "073456781"]
+    phonebook.extract("07123456789, 07123456788")
+    expect(phonebook.list).to eq ["07123456789", "07123456788"]
   end
 
-  #4. calling our extraction method with words included, extracts just the phone number
-  xit "calling our extraction method with words included, extracts just the phone number" do
+  #4. calling our extraction method with words included, 
+  # extracts just the phone number
+  it "calling our extraction method with words included, 
+         extracts just the phone number" do
     phonebook = Phonebook.new
-    phonebook.extract("my friend's number is 073456789") 
-    expect(phonebook.list).to eq ["073456789"]
+    phonebook.extract("my friend's number is 07123456789") 
+    expect(phonebook.list).to eq ["07123456789"]
   end
 
-  # 5. calling our extraction method again, extracts new number 
-  # in a list with previously extracted numbers
-  xit "calling our extraction method again, extracts new number 
-      in a list with previously extracted numbers" do 
+  # 5. calling our extraction method again, extracts new
+  # number in a list with previously extracted numbers
+  it "calling our extraction method again, extracts new
+      number in a list with previously extracted numbers" do 
     phonebook = Phonebook.new
-    phonebook.extract("073456789")
-    phonebook.extract("073456781")
-    expect(phonebook.list).to eq ["073456789", "073456781"]
+    phonebook.extract("07123456789")
+    phonebook.extract("07123456788")
+    expect(phonebook.list).to eq ["07123456789", "07123456788"]
   end 
 
   # 6. extract ignores duplicates from 2 extractions 
-  xit "extract ignores duplicates from 2 extractions " do
+  it "extract ignores duplicates from 2 extractions " do
     phonebook = Phonebook.new
-    phonebook.extract("073456789")
-    phonebook.extract("new numbers 073456789 073456781")
-    phonebook.list .to eq  ["073456789", "073456781"]
+    phonebook.extract("07123456789")
+    phonebook.extract("new numbers 07123456789 07123456788")
+    expect(phonebook.list).to eq ["07123456789", "07123456788"]
   end
 
   # 7. extract ignores duplicates from a single diary entry
-  xit "extract method ignores duplicates from a single diary entry" do
+  xit "extract method ignores duplicates from a single diary
+       entry" do
     phonebook = Phonebook.new
-    phonebook.extract("073456789, 073456789, 073456781")
-    expect(phonebook.list).to eq ["073456789", "073456781"]
+    phonebook.extract("07123456789, 07123456789")
+    expect(phonebook.list).to eq ["07123456789"]
   end
 
   #8. extract throws error from messages that don't
@@ -68,6 +72,6 @@ RSpec.describe Phonebook do
   # 9. throws error if the number is the wrong length
   xit "number is the wrong length" do
     phonebook = Phonebook.new
-    expect{ phonebook.extract("07345678")}.to raise_error "number is the wrong length!" 
+    expect{ phonebook.extract("0712345678")}.to raise_error "Number is the wrong length!" 
   end
 end
